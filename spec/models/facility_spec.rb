16f8d071429f404ead85fb570e5e67cd170e9271
Susha Dore <susha.dore@gmail.com>
2017-07-21 11:27:30 -0700

    pass updated facility model spec

--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Facility do
   it { should belong_to :park }
-  it { should validate_presence_of :type }
+  it { should validate_presence_of :amenity }
   it { should validate_presence_of :description }
 end
