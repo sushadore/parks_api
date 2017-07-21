7e4d244aa9c80c2de4be44f5e7140dc76a1cdfc5
Susha Dore <susha.dore@gmail.com>
2017-07-21 14:02:29 -0700

    pass post park spec

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-context "get all parks route", :type => :request do
+describe "get parks route", :type => :request do
   let!(:parks) { FactoryGirl.create_list(:park, 13)}
-
 
   before { get "/parks" }
 
-  it 'returns all parks' do
+  it "returns all parks" do
     expect(JSON.parse(response.body).size).to eq 13
   end
 
-  it 'returns status code 200' do
-    expect(response).to have_http_status :success
+  it "returns success status" do
+    expect(response).to have_http_status 200
   end
 end
