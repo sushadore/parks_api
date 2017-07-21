37ac8c12ebd4ef8e39cd56f987532c559d901915
Susha Dore <susha.dore@gmail.com>
2017-07-21 13:34:53 -0700

    add crud for parks and facilities

---
+++ app/controllers/facilities_controller.rb
@@ -0,0 +1,41 @@
+class FacilitiesController < ApplicationController
+
+  def index
+    @park = Park.find(params[:park_id])
+    json_response(@park.facilities)
+  end
+
+  def show
+    @facility = Facility.find(params[:id])
+    json_response(@facility)
+  end
+
+  def create
+    @park = Park.find(params[:park_id])
+    @facility = @park.facilities.create!(facility_params)
+    json_response(@facility.park)
+  end
+
+  def update
+    @facility = Facility.find(params[:id])
+    if @facility.update!(facility_params)
+      render status: 200, json: {
+        message: "Facility has been updated."
+      }
+    end
+  end
+
+  def destroy
+    @facility = Facility.find(params[:id])
+    @facility.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def facility_params
+    params.permit(:amenity, :description)
+  end
+end
---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,48 @@
+class ParksController < ApplicationController
+
+  def index
+    @parks = Park.all
+    # title = params[:title]
+    # @parks = Park.title_search(title)
+    json_response(@parks.page(params[:page]))
+
+  end
+
+  def show
+    park = Park.find(params[:id])
+    json_response(park)
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    json_response(@park, :accepted)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "Park has been updated."
+      }
+    end
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
+    if @park.destroy!
+      render status: 200, json: {
+        message: "The park has been deleted."
+      }
+  end
+end
+
+private
+  def json_response(object, status = :ok)
+    render json: object.as_json(include: :facilities), status: status
+  end
+
+  def park_params
+    params.permit(:name, :category, :location)
+  end
+end
