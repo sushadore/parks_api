808436c63d0802a228c387700e27589c049f3b81
Susha Dore <susha.dore@gmail.com>
2017-07-21 10:07:44 -0700

    pass park model testing

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Park, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Park do
+  it { should have_many :facilities }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :category }
+  it { should validate_presence_of :location }
 end
