254d517feb936a43ad85bf8345043f0e618c8c4c
Susha Dore <susha.dore@gmail.com>
2017-07-21 15:15:03 -0700

    add validation exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do |exception|
     json_response({ message: exception.message }, :not_found)
   end
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({ message: exception.message }, :unprocessable_entity)
+  end
 end
